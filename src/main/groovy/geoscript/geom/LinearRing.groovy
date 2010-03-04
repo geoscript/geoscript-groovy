97835ee08bee882524fbaf7ad2c633a7515df191
Jared Erickson <jared.erickson@gmail.com>
2010-03-03 20:47:58 -0800

    Accept a List of Points and a List of List of doubles when creating a LinearRing

--- src/main/groovy/geoscript/geom/LinearRing.groovy
+++ src/main/groovy/geoscript/geom/LinearRing.groovy
@@ -18,8 +18,9 @@ class LinearRing extends Geometry {
     /**
      * Create a new LinearRing with a List of List of Doubles.
      * <p>LinearRing ring = new LinearRing([[1,2],[3,4],[4,5]])</p>
+     * <p>LinearRing ring = new LinearRing([new Point(111.0, -47), new Point(123.0, -48), new Point(110.0, -47), new Point(111.0, -47)])</p>
      */
-    LinearRing(List<List<Double>> coordinates) {
+    LinearRing(List coordinates) {
         this(create(coordinates))
     }
 
@@ -35,8 +36,10 @@ class LinearRing extends Geometry {
      * Create a new LinearRing with a List of List of Doubles.
      * <p>LinearRing ring = new LinearRing([[1,2],[3,4],[4,5]])</p>
      */
-    private static JtsLinearRing create(List<List<Double>> coordinates) {
-        Geometry.factory.createLinearRing(coordinates.collect{new Coordinate(it[0], it[1])}.toArray() as Coordinate[])
+    private static JtsLinearRing create(List coordinates) {
+        Geometry.factory.createLinearRing(coordinates.collect{c ->
+                (c instanceof Point) ? c.g.coordinate : new Coordinate(c[0], c[1])
+        }.toArray() as Coordinate[])
     }
 	
     /**
--- src/test/groovy/geoscript/geom/LinearRingTestCase.groovy
+++ src/test/groovy/geoscript/geom/LinearRingTestCase.groovy
@@ -7,10 +7,13 @@ class LinearRingTestCase {
 	
 	@Test void constructors() {
 		def l1 = new LinearRing([[111.0, -47],[123.0, -48],[110.0, -47], [111.0, -47]])
-		assertEquals "LINEARRING (111 -47, 123 -48, 110 -47, 111 -47)", l1.wkt()
-		
-		def l2 = new LinearRing([111.0, -47],[123.0, -48],[110.0, -47], [111.0, -47])
-		assertEquals "LINEARRING (111 -47, 123 -48, 110 -47, 111 -47)", l2.wkt()
+		assertEquals "LINEARRING (111 -47, 123 -48, 110 -47, 111 -47)", l1.wkt
+
+                def l2 = new LinearRing([new Point(111.0, -47), new Point(123.0, -48), new Point(110.0, -47), new Point(111.0, -47)])
+		assertEquals "LINEARRING (111 -47, 123 -48, 110 -47, 111 -47)", l2.wkt
+
+		def l3 = new LinearRing([111.0, -47],[123.0, -48],[110.0, -47], [111.0, -47])
+		assertEquals "LINEARRING (111 -47, 123 -48, 110 -47, 111 -47)", l3.wkt
 	}
 	
 }
