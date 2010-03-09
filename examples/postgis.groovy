210a020c0af309c03dba66f0220d2756bbd8a535
Jared Erickson <jared.erickson@gmail.com>
2010-03-08 19:25:34 -0800

    Adding postgis example.

---
+++ examples/postgis.groovy
@@ -0,0 +1,35 @@
+import geoscript.layer.Layer
+import geoscript.workspace.PostGIS
+
+// Connect to a PostGIS database
+def p = new PostGIS('postgres','localhost','5432','public','postgres','postgres')
+
+// Print the format
+println("Format: $p.format")
+
+// Print the layer names
+println("Layers:")
+p.layers.each{lyr -> println(lyr)}
+
+// Get the first Layer
+Layer layer = p.get(p.layers[0])
+println("Layer: ${layer.name}")
+println("Projection : ${layer.proj}")
+println("# Features: ${layer.count()}")
+println("Bounds: ${layer.bounds()}")
+
+// Print each feature in the first Layer
+println("Features:")
+layer.features.each{f -> println(f)}
+
+// Get a cursor to iterate over each Feature
+println("Features from Cursor:")
+def c = layer.cursor
+while(c.hasNext()) {
+    def f = c.next()
+    println(f)
+}
+c.close()
+
+// Export as GML
+layer.toGML()
