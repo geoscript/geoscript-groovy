677bb2e4edb96563bff049249294a1a151b9e84e
Jared Erickson <jared.erickson@gmail.com>
2010-10-12 19:47:34 -0700

    First attempt at valign and halign text

--- src/main/groovy/geoscript/print/TextItem.groovy
+++ src/main/groovy/geoscript/print/TextItem.groovy
@@ -13,21 +13,32 @@ class TextItem extends Item {
     String text
     Color color = Color.BLACK
     Font font = new Font("Default", Font.PLAIN, 12)
-    String align = "left" // left, center, right
+    String halign = "left" // left, center, right
+    String valign = "bottom" // top, middle, bottom
 
     void draw(Graphics g) {
         g.color = color
         g.font = font
         int drawX = x
         int drawY = y
-        if (width > 0 && !align.equalsIgnoreCase("left")) {
+        if (width > 0 && !halign.equalsIgnoreCase("left")) {
             def metrics = g.fontMetrics
             int stringWidth = metrics.stringWidth(text)
-            if (align.equalsIgnoreCase("center")) {
-                drawX = width / 2 - stringWidth / 2
+            if (halign.equalsIgnoreCase("center")) {
+                drawX = x + (width / 2 - stringWidth / 2)
             }
-            else if (align.equalsIgnoreCase("right")) {
-                drawX = width - stringWidth
+            else if (halign.equalsIgnoreCase("right")) {
+                drawX = x+ (width - stringWidth)
+            }
+        }
+        if (height > 0 && !valign.equalsIgnoreCase("bottom")) {
+            def metrics = g.fontMetrics
+            int stringHeight = metrics.getStringBounds(text, g).height
+            if (valign.equalsIgnoreCase("middle")) {
+                drawX = height / 2 - stringHeight / 2
+            }
+            else if (valign.equalsIgnoreCase("top")) {
+                drawX = height + stringHeight
             }
         }
         g.drawString(text, drawX, drawY)
