0a23a0de025660e05d6eaba2796b9de838b2bb51
Jared Erickson <jared.erickson@gmail.com>
2011-07-26 21:45:01 -0700

    Add a Property Workspace.

---
+++ src/main/groovy/geoscript/workspace/Property.groovy
@@ -0,0 +1,48 @@
+package geoscript.workspace
+
+import org.geotools.data.DataStore
+import org.geotools.data.property.PropertyDataStoreFactory
+
+/**
+ * A Workspace based on a directory of java style property files.
+ * @author Jared Erickson
+ */
+class Property extends Workspace {
+
+    /**
+     * Create a new Property Workspace
+     * @param directory The File directory
+     */
+    Property(File directory) {
+        super(createDataStore(directory))
+    }
+
+    /**
+     * Create a new Property Workspace with a directory
+     * @param directory The directory
+     */
+    private static DataStore createDataStore(File directory) {
+        Map params = new java.util.HashMap()
+        params.put("directory", directory.absolutePath)
+        PropertyDataStoreFactory factory = new PropertyDataStoreFactory()
+        factory.createDataStore(params)
+    }
+
+    /**
+     * Get the format
+     * @return The workspace format name
+     */
+    @Override
+    String getFormat() {
+        return "Property"
+    }
+
+    /**
+     * Get the string representation
+     * @return The string representation
+     */
+    @Override
+    String toString() {
+        return "Property[${ds.info.source.path}]"
+    }
+}
