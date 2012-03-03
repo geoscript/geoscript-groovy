c8ef355ad4b73107a20d7fd04d8260eef1fb309b
Jared Erickson <jared.erickson@gmail.com>
2012-03-03 13:24:51 -0800

    Test of the Cursor reset method.

--- src/test/groovy/geoscript/layer/CursorTestCase.groovy
+++ src/test/groovy/geoscript/layer/CursorTestCase.groovy
@@ -35,5 +35,29 @@ class CursorTestCase {
         c.close()
         assertEquals 49, names.size()
     }
+
+    @Test void reset() {
+
+        // Get a Shapefle
+        File file = new File(getClass().getClassLoader().getResource("states.shp").toURI())
+        assertNotNull(file)
+        Shapefile shp = new Shapefile(file)
+        assertNotNull(shp)
+
+        // Use Cursor to get a List of NAME values
+        Cursor c = shp.cursor
+        def names = c.collect{f -> f.get("NAME")}
+        c.close()
+        assertEquals 49, names.size()
+
+        // Reset the Cursor to read again
+        c.reset()
+
+        // Use Cursor to get a List of NAME values again
+        names = c.collect{f -> f.get("NAME")}
+        c.close()
+        assertEquals 49, names.size()
+    }
+
 }
 
