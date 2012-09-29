d8f8941aee09ed15d68bf85270d627ec7558fa71
Jared Erickson <jared.erickson@gmail.com>
2012-09-28 19:18:14 -0700

    Improve example to use a function for the label instead of a property.  Yea filters!

--- examples/map_states_function.groovy
+++ examples/map_states_function.groovy
@@ -12,7 +12,7 @@ statesShp.style = (new Fill("#E6E6E6") + new Stroke("#4C4C4C",0.5)) +
         (new Shape("#66CCff", 6, "circle").stroke("#004080") + new Transform(
                 myCentroid
         )).zindex(1) +
-        (new Label("STATE_ABBR").font(new Font("normal", "bold", 10, "serif")).fill(new Fill("#004080")))
+        (new Label(new Function("strToLowerCase(STATE_ABBR)")).font(new Font("normal", "bold", 10, "serif")).fill(new Fill("#004080")))
 
 def map = new Map(width: 600, height: 400, fixAspectRatio: true)
 map.proj = "EPSG:4326"
