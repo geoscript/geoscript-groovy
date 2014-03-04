4cc913870c7a74f3921d7c08f173dbac339f86fb
Jared Erickson <jared.erickson@gmail.com>
2014-03-03 19:51:34 -0800

    The H2 Workspace can now connect to GeoDB databases running in server mode (geode -w) using database, host, port, schema, user, password params or JNDI reference name for web apps.

--- src/main/groovy/geoscript/workspace/H2.groovy
+++ src/main/groovy/geoscript/workspace/H2.groovy
@@ -2,6 +2,7 @@ package geoscript.workspace
 
 import org.geotools.data.DataStore
 import org.geotools.data.h2.H2DataStoreFactory
+import org.geotools.data.h2.H2JNDIDataStoreFactory
 
 /**
  * A H2 Workspace connects to a spatially enabled H2 database.
@@ -21,7 +22,7 @@ class H2 extends Database {
      * @param name The name of the database
      * @param dir The File containing the database
      */
-    H2 (String name, File dir) {
+    H2(String name, File dir) {
         super(createDataStore(name, dir))
     }
 
@@ -30,7 +31,7 @@ class H2 extends Database {
      * @param name The name of the database
      * @param dir The File containing the database
      */
-    H2 (String name, String dir) {
+    H2(String name, String dir) {
         this(name, new File(dir).absoluteFile)
     }
 
@@ -38,8 +39,38 @@ class H2 extends Database {
      * Create a new H2 Workspace from a database file
      * @param file The H2 database file
      */
-    H2 (File file) {
+    H2(File file) {
         this(file.name, file.parentFile)
+    }
+
+    /**
+     * Create a new H2 Workspace with a name, host, port, schema, user, and password.
+     * @param database The database name
+     * @param host The host
+     * @param port The port
+     * @param schema The schema
+     * @param user The user name
+     * @param password The password
+     */
+    H2(String database, String host, String port, String schema, String user, String password) {
+        super(createDataStore(database, host, port, schema, user, password))
+    }
+
+    /**
+     * Create a new H2 Workspace
+     * @param options The optional named parameters
+     * <ul>
+     *     <li>host = The host (defaults to localhost)</li>
+     *     <li>port = The port (defaults to an empty string)</li>
+     *     <li>schema = The schema (defaults to null)</li>
+     *     <li>user = The user name (defaults to sa)</li>
+     *     <li>password = The password (defaults to an empty string)</li>
+     * </ul>
+     * @param database The database name (or JNDI reference name)
+     */
+    H2(Map options = [:], String database) {
+        this(database, options.get("host", "localhost"), options.get("port", ""), options.get("schema"),
+                options.get("user", "sa"), options.get("password", ""))
     }
 
     /**
@@ -56,9 +87,32 @@ class H2 extends Database {
      */
     private static DataStore createDataStore(String name, File dir) {
         Map params = new java.util.HashMap()
-        params.put("database", new File(dir,name).absolutePath)
+        params.put("database", new File(dir, name).absolutePath)
         params.put("dbtype", "h2")
         H2DataStoreFactory h2f = new H2DataStoreFactory()
+        h2f.createDataStore(params)
+    }
+
+    /**
+     * Create a new H2 Workspace with a TCP connections
+     */
+    private static DataStore createDataStore(String database, String host, String port, String schema, String user, String password) {
+        def h2f
+        Map params = new java.util.HashMap()
+        params.put("dbtype", "h2")
+        if (database.startsWith("java:comp/env/")) {
+            h2f = new H2JNDIDataStoreFactory()
+            params.put("jndiReferenceName", database)
+            params.put("schema", schema)
+        } else {
+            h2f = new H2DataStoreFactory()
+            params.put("database", database)
+            params.put("schema", schema)
+            params.put("host", host)
+            params.put("port", port)
+            params.put("user", user)
+            params.put("passwd", password)
+        }
         h2f.createDataStore(params)
     }
 }
