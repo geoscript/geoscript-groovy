57d4e07c5c475e8b8824e095d9842f498cfe27ba
jericks <jared.erickson@gmail.com>
2014-06-04 19:48:18 -0700

    Remove fully qualified assert statements

--- src/test/groovy/geoscript/tile/MbTilesTestCase.groovy
+++ src/test/groovy/geoscript/tile/MbTilesTestCase.groovy
@@ -22,23 +22,23 @@ class MbTilesTestCase {
         File file = new File(getClass().getClassLoader().getResource("states.mbtiles").toURI())
         MBTiles layer = new MBTiles(file)
         Bounds b = new Bounds(-179.99, -85.0511, 179.99, 85.0511, "EPSG:4326").reproject("EPSG:3857")
-        junit.framework.Assert.assertEquals "states", layer.name
-        org.junit.Assert.assertEquals new Projection("EPSG:3857"), layer.proj
-        org.junit.Assert.assertEquals b, layer.bounds
+        assertEquals "states", layer.name
+        assertEquals new Projection("EPSG:3857"), layer.proj
+        assertEquals b, layer.bounds
         Pyramid pyramid = layer.pyramid
-        junit.framework.Assert.assertEquals "EPSG:3857", pyramid.proj.id
-        org.junit.Assert.assertEquals b, pyramid.bounds
-        junit.framework.Assert.assertEquals 256, pyramid.tileWidth
-        junit.framework.Assert.assertEquals 256, pyramid.tileHeight
-        org.junit.Assert.assertEquals Pyramid.Origin.BOTTOM_LEFT, pyramid.origin
-        junit.framework.Assert.assertEquals 20, pyramid.grids.size()
+        assertEquals "EPSG:3857", pyramid.proj.id
+        assertEquals b, pyramid.bounds
+        assertEquals 256, pyramid.tileWidth
+        assertEquals 256, pyramid.tileHeight
+        assertEquals Pyramid.Origin.BOTTOM_LEFT, pyramid.origin
+        assertEquals 20, pyramid.grids.size()
         pyramid.grids.eachWithIndex { Grid g, int z ->
-            org.junit.Assert.assertEquals z, g.z
+            assertEquals z, g.z
             int n = Math.pow(2, z)
-            junit.framework.Assert.assertEquals n, g.width
-            junit.framework.Assert.assertEquals n, g.height
-            org.junit.Assert.assertEquals 156412.0 / n, g.xResolution, 0.01
-            org.junit.Assert.assertEquals 156412.0 / n, g.yResolution, 0.01
+            assertEquals n, g.width
+            assertEquals n, g.height
+            assertEquals 156412.0 / n, g.xResolution, 0.01
+            assertEquals 156412.0 / n, g.yResolution, 0.01
         }
         layer.close()
     }
@@ -47,11 +47,11 @@ class MbTilesTestCase {
         File file = new File(getClass().getClassLoader().getResource("states.mbtiles").toURI())
         MBTiles layer = new MBTiles(file)
         Tile tile = layer.get(4, 2, 3)
-        org.junit.Assert.assertNotNull tile
-        org.junit.Assert.assertEquals 4, tile.z
-        org.junit.Assert.assertEquals 2, tile.x
-        org.junit.Assert.assertEquals 3, tile.y
-        org.junit.Assert.assertNotNull tile.data
+        assertNotNull tile
+        assertEquals 4, tile.z
+        assertEquals 2, tile.x
+        assertEquals 3, tile.y
+        assertNotNull tile.data
         layer.close()
     }
 
@@ -68,11 +68,11 @@ class MbTilesTestCase {
 
         // Make sure Tile doesn't exist in database
         Tile tile = layer.get(10, 0, 0)
-        org.junit.Assert.assertNotNull tile
-        org.junit.Assert.assertEquals 10, tile.z
-        org.junit.Assert.assertEquals 0, tile.x
-        org.junit.Assert.assertEquals 0, tile.y
-        junit.framework.Assert.assertNull tile.data
+        assertNotNull tile
+        assertEquals 10, tile.z
+        assertEquals 0, tile.x
+        assertEquals 0, tile.y
+        assertNull tile.data
 
         // Load a tile image
         File f = new File(getClass().getClassLoader().getResource("0.png").toURI())
@@ -81,11 +81,11 @@ class MbTilesTestCase {
         // Save Tile and make sure it saved correctly by getting it again
         layer.put(tile)
         tile = layer.get(10, 0, 0)
-        org.junit.Assert.assertNotNull tile
-        org.junit.Assert.assertEquals 10, tile.z
-        org.junit.Assert.assertEquals 0, tile.x
-        org.junit.Assert.assertEquals 0, tile.y
-        org.junit.Assert.assertNotNull tile.data
+        assertNotNull tile
+        assertEquals 10, tile.z
+        assertEquals 0, tile.x
+        assertEquals 0, tile.y
+        assertNotNull tile.data
         layer.close()
     }
 
@@ -93,21 +93,21 @@ class MbTilesTestCase {
         File file = new File(getClass().getClassLoader().getResource("states.mbtiles").toURI())
         MBTiles layer = new MBTiles(file)
         TileCursor cursor = layer.tiles(1)
-        org.junit.Assert.assertEquals 1, cursor.z
-        org.junit.Assert.assertEquals 0, cursor.minX
-        org.junit.Assert.assertEquals 0, cursor.minY
-        org.junit.Assert.assertEquals 1, cursor.maxX
-        org.junit.Assert.assertEquals 1, cursor.maxY
-        org.junit.Assert.assertEquals 2, cursor.width
-        org.junit.Assert.assertEquals 2, cursor.height
-        org.junit.Assert.assertEquals 4, cursor.size
+        assertEquals 1, cursor.z
+        assertEquals 0, cursor.minX
+        assertEquals 0, cursor.minY
+        assertEquals 1, cursor.maxX
+        assertEquals 1, cursor.maxY
+        assertEquals 2, cursor.width
+        assertEquals 2, cursor.height
+        assertEquals 4, cursor.size
         int c = 0
         cursor.each{ Tile t ->
-            org.junit.Assert.assertEquals 1, t.z
-            org.junit.Assert.assertNotNull t.data
+            assertEquals 1, t.z
+            assertNotNull t.data
             c++
         }
-        junit.framework.Assert.assertEquals 4, c
+        assertEquals 4, c
         layer.close()
     }
 
@@ -115,21 +115,21 @@ class MbTilesTestCase {
         File file = new File(getClass().getClassLoader().getResource("states.mbtiles").toURI())
         MBTiles layer = new MBTiles(file)
         TileCursor cursor = layer.tiles(2, 1, 2, 3, 3)
-        org.junit.Assert.assertEquals 2, cursor.z
-        org.junit.Assert.assertEquals 1, cursor.minX
-        org.junit.Assert.assertEquals 2, cursor.minY
-        org.junit.Assert.assertEquals 3, cursor.maxX
-        org.junit.Assert.assertEquals 3, cursor.maxY
-        org.junit.Assert.assertEquals 3, cursor.width
-        org.junit.Assert.assertEquals 2, cursor.height
-        org.junit.Assert.assertEquals 6, cursor.size
+        assertEquals 2, cursor.z
+        assertEquals 1, cursor.minX
+        assertEquals 2, cursor.minY
+        assertEquals 3, cursor.maxX
+        assertEquals 3, cursor.maxY
+        assertEquals 3, cursor.width
+        assertEquals 2, cursor.height
+        assertEquals 6, cursor.size
         int c = 0
         cursor.each{ Tile t ->
-            org.junit.Assert.assertEquals 2, t.z
-            org.junit.Assert.assertNotNull t.data
+            assertEquals 2, t.z
+            assertNotNull t.data
             c++
         }
-        junit.framework.Assert.assertEquals 6, c
+        assertEquals 6, c
         layer.close()
     }
 
@@ -138,21 +138,21 @@ class MbTilesTestCase {
         MBTiles layer = new MBTiles(file)
         Bounds b = new Bounds(-123.09, 46.66, -121.13, 47.48, "EPSG:4326").reproject("EPSG:3857")
         TileCursor cursor = layer.tiles(b, 3)
-        org.junit.Assert.assertEquals 3, cursor.z
-        org.junit.Assert.assertEquals 1, cursor.minX
-        org.junit.Assert.assertEquals 5, cursor.minY
-        org.junit.Assert.assertEquals 1, cursor.maxX
-        org.junit.Assert.assertEquals 5, cursor.maxY
-        org.junit.Assert.assertEquals 1, cursor.width
-        org.junit.Assert.assertEquals 1, cursor.height
-        org.junit.Assert.assertEquals 1, cursor.size
+        assertEquals 3, cursor.z
+        assertEquals 1, cursor.minX
+        assertEquals 5, cursor.minY
+        assertEquals 1, cursor.maxX
+        assertEquals 5, cursor.maxY
+        assertEquals 1, cursor.width
+        assertEquals 1, cursor.height
+        assertEquals 1, cursor.size
         int c = 0
         cursor.each{ Tile t ->
-            org.junit.Assert.assertEquals 3, t.z
-            org.junit.Assert.assertNotNull t.data
+            assertEquals 3, t.z
+            assertNotNull t.data
             c++
         }
-        junit.framework.Assert.assertEquals 1, c
+        assertEquals 1, c
         layer.close()
     }
 
@@ -161,21 +161,21 @@ class MbTilesTestCase {
         MBTiles layer = new MBTiles(file)
         Bounds b = new Bounds(-124.73142200000001, 24.955967, -66.969849, 49.371735, "EPSG:4326").reproject("EPSG:3857")
         TileCursor cursor = layer.tiles(b, b.width / 400, b.height / 300)
-        org.junit.Assert.assertEquals 4, cursor.z
-        org.junit.Assert.assertEquals 2, cursor.minX
-        org.junit.Assert.assertEquals 9, cursor.minY
-        org.junit.Assert.assertEquals 5, cursor.maxX
-        org.junit.Assert.assertEquals 10, cursor.maxY
-        org.junit.Assert.assertEquals 4, cursor.width
-        org.junit.Assert.assertEquals 2, cursor.height
-        org.junit.Assert.assertEquals 8, cursor.size
+        assertEquals 4, cursor.z
+        assertEquals 2, cursor.minX
+        assertEquals 9, cursor.minY
+        assertEquals 5, cursor.maxX
+        assertEquals 10, cursor.maxY
+        assertEquals 4, cursor.width
+        assertEquals 2, cursor.height
+        assertEquals 8, cursor.size
         int c = 0
         cursor.each{ Tile t ->
-            org.junit.Assert.assertEquals 4, t.z
-            org.junit.Assert.assertNotNull t.data
+            assertEquals 4, t.z
+            assertNotNull t.data
             c++
         }
-        junit.framework.Assert.assertEquals 8, c
+        assertEquals 8, c
         layer.close()
     }
 
@@ -184,21 +184,21 @@ class MbTilesTestCase {
         MBTiles layer = new MBTiles(file)
         Bounds b = new Bounds(-124.73142200000001, 24.955967, -66.969849, 49.371735, "EPSG:4326").reproject("EPSG:3857")
         TileCursor cursor = layer.tiles(b, 400, 300)
-        org.junit.Assert.assertEquals 4, cursor.z
-        org.junit.Assert.assertEquals 2, cursor.minX
-        org.junit.Assert.assertEquals 9, cursor.minY
-        org.junit.Assert.assertEquals 5, cursor.maxX
-        org.junit.Assert.assertEquals 10, cursor.maxY
-        org.junit.Assert.assertEquals 4, cursor.width
-        org.junit.Assert.assertEquals 2, cursor.height
-        org.junit.Assert.assertEquals 8, cursor.size
+        assertEquals 4, cursor.z
+        assertEquals 2, cursor.minX
+        assertEquals 9, cursor.minY
+        assertEquals 5, cursor.maxX
+        assertEquals 10, cursor.maxY
+        assertEquals 4, cursor.width
+        assertEquals 2, cursor.height
+        assertEquals 8, cursor.size
         int c = 0
         cursor.each{ Tile t ->
-            org.junit.Assert.assertEquals 4, t.z
-            org.junit.Assert.assertNotNull t.data
+            assertEquals 4, t.z
+            assertNotNull t.data
             c++
         }
-        junit.framework.Assert.assertEquals 8, c
+        assertEquals 8, c
         layer.close()
     }
 
@@ -207,10 +207,10 @@ class MbTilesTestCase {
         MBTiles layer = new MBTiles(file)
         Bounds b = new Bounds(-124.73142200000001, 24.955967, -66.969849, 49.371735, "EPSG:4326").reproject("EPSG:3857")
         Map coords = layer.getTileCoordinates(b, layer.pyramid.grid(4))
-        junit.framework.Assert.assertEquals 2, coords.minX
-        junit.framework.Assert.assertEquals 9, coords.minY
-        junit.framework.Assert.assertEquals 5, coords.maxX
-        junit.framework.Assert.assertEquals 10, coords.maxY
+        assertEquals 2, coords.minX
+        assertEquals 9, coords.minY
+        assertEquals 5, coords.maxX
+        assertEquals 10, coords.maxY
         layer.close()
     }
 
@@ -219,12 +219,12 @@ class MbTilesTestCase {
         MBTiles layer = new MBTiles(file)
         Bounds b = new Bounds(-124.73142200000001, 24.955967, -66.969849, 49.371735, "EPSG:4326").reproject("EPSG:3857")
         Raster raster = layer.getRaster(layer.tiles(b, 4))
-        org.junit.Assert.assertNotNull raster
+        assertNotNull raster
         File out = folder.newFile("raster.png")
         WorldImage format = new WorldImage(out)
         format.write(raster)
-        junit.framework.Assert.assertTrue out.exists()
-        junit.framework.Assert.assertTrue out.length() > 0
+        assertTrue out.exists()
+        assertTrue out.length() > 0
         layer.close()
     }
 
@@ -233,12 +233,12 @@ class MbTilesTestCase {
         MBTiles layer = new MBTiles(file)
         Bounds b = new Bounds(-124.73142200000001, 24.955967, -66.969849, 49.371735, "EPSG:4326").reproject("EPSG:3857")
         Raster raster = layer.getRaster(b, 400, 300)
-        org.junit.Assert.assertNotNull raster
+        assertNotNull raster
         File out = folder.newFile("raster.png")
         WorldImage format = new WorldImage(out)
         format.write(raster)
-        junit.framework.Assert.assertTrue out.exists()
-        junit.framework.Assert.assertTrue out.length() > 0
+        assertTrue out.exists()
+        assertTrue out.length() > 0
         layer.close()
     }
 
