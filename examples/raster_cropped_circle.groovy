a204b2c5b71c7302308f51cd3ad97b25c89d5c78
jericks <jared.erickson@gmail.com>
2014-09-13 17:53:13 -0700

    Fix the raster cropped circle example

--- examples/raster_cropped_circle.groovy
+++ examples/raster_cropped_circle.groovy
@@ -3,13 +3,13 @@ import geoscript.geom.Point
 import geoscript.proj.Projection
 
 // Read
-def geoTiffFormat = new GeoTIFF(new File("raster.tif"))
-def raster = geoTiffFormat.read(new Projection("urn:ogc:def:crs:EPSG::EPSG:4326"))
+def geoTiffFormat = new GeoTIFF(new File("alki.tif"))
+def raster = geoTiffFormat.read(new Projection("urn:ogc:def:crs:EPSG::EPSG:2927"))
 
 // Crop
-def geom = new Point(50,50).buffer(20)
+def geom = new Point(150,150).buffer(20)
 def croppedRaster = raster.crop(geom)
 
 // Write
 GeoTIFF croppedGeoTIFF = new GeoTIFF(new File("raster_circle_cropped.tif"))
-geoTiffFormat.write(croppedRaster)
+croppedGeoTIFF.write(croppedRaster)
