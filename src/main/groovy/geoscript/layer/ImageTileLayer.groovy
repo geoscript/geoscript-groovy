198e55c9616adc084c3eefa34565e2e546917ea4
jericks <jared.erickson@gmail.com>
2014-10-28 19:55:16 -0700

    When empty TileCursor will return a blank raster.  ImageTileLayer should handle this gracefully

--- src/main/groovy/geoscript/layer/ImageTileLayer.groovy
+++ src/main/groovy/geoscript/layer/ImageTileLayer.groovy
@@ -12,44 +12,55 @@ import java.awt.image.BufferedImage
 abstract class ImageTileLayer extends TileLayer<ImageTile> {
 
     /**
-     * Get a Raster using Tiles from the TileCursor
+     * Get a Raster using Tiles from the TileCursor.
+     * If the TileCursor is empty a null Raster will be returned
      * @param cursor The TileCursor
      * @return A Raster
      */
     Raster getRaster(TileCursor<ImageTile> cursor) {
-        Bounds tileBounds = cursor.bounds
-        int imageWidth = cursor.width * pyramid.tileWidth
-        int imageHeight =  cursor.height * pyramid.tileHeight
-        BufferedImage image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB)
-        Graphics2D g2d = image.createGraphics()
-        cursor.each{ ImageTile tile ->
-            int x
-            if (pyramid.origin == Pyramid.Origin.TOP_LEFT || pyramid.origin == Pyramid.Origin.BOTTOM_LEFT) {
-                x = (tile.x - cursor.minX) * pyramid.tileWidth
-            } else {
-                x = (cursor.maxX - tile.x) * pyramid.tileWidth
+        // Make sure the TileCursor is not empty
+        if (!cursor.empty) {
+            Bounds tileBounds = cursor.bounds
+            int imageWidth = cursor.width * pyramid.tileWidth
+            int imageHeight = cursor.height * pyramid.tileHeight
+            BufferedImage image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB)
+            Graphics2D g2d = image.createGraphics()
+            cursor.each { ImageTile tile ->
+                int x
+                if (pyramid.origin == Pyramid.Origin.TOP_LEFT || pyramid.origin == Pyramid.Origin.BOTTOM_LEFT) {
+                    x = (tile.x - cursor.minX) * pyramid.tileWidth
+                } else {
+                    x = (cursor.maxX - tile.x) * pyramid.tileWidth
+                }
+                int y
+                if (pyramid.origin == Pyramid.Origin.TOP_LEFT || pyramid.origin == Pyramid.Origin.TOP_RIGHT) {
+                    y = (tile.y - cursor.minY) * pyramid.tileHeight
+                } else {
+                    y = (cursor.maxY - tile.y) * pyramid.tileHeight
+                }
+                g2d.drawImage(tile.image, x, y, pyramid.tileWidth, pyramid.tileHeight, null)
             }
-            int y
-            if (pyramid.origin == Pyramid.Origin.TOP_LEFT || pyramid.origin == Pyramid.Origin.TOP_RIGHT) {
-                y = (tile.y - cursor.minY) * pyramid.tileHeight
-            } else {
-                y = (cursor.maxY - tile.y) * pyramid.tileHeight
-            }
-            g2d.drawImage(tile.image, x, y, pyramid.tileWidth, pyramid.tileHeight, null)
+            g2d.dispose()
+            return new Raster(image, tileBounds)
+        } else {
+            return null
         }
-        g2d.dispose()
-        new Raster(image, tileBounds)
     }
 
     /**
      * Get a Raster using Tiles for the Bounds and image size.  The Raster is cropped
-     * to exactly the given Bounds
+     * to exactly the given Bounds.
      * @param b Bounds
      * @param w The image width
      * @param h The image height
      * @return A Raster
      */
     Raster getRaster(Bounds b, int w, int h) {
-        getRaster(tiles(b,w,h)).crop(b)
+        TileCursor c = tiles(b,w,h)
+        if (!c.empty) {
+            getRaster(c).crop(b)
+        } else {
+            new Raster(new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB), b)
+        }
     }
 }
