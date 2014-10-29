aa042bb9c945b1e1b27f2d7f63258d340d6ce849
jericks <jared.erickson@gmail.com>
2014-10-28 19:56:27 -0700

    The TileGenerator can now generate tiles that intersect a bounds

--- src/main/groovy/geoscript/layer/TileGenerator.groovy
+++ src/main/groovy/geoscript/layer/TileGenerator.groovy
@@ -15,16 +15,21 @@ class TileGenerator {
 
     /**
      * Generate Tiles for a TileLayer using a TileRenderer
+     * @param options The optional named parameters:
+     * <ul>
+     *     <li>bounds = The Bounds of the Tiles to generate</li>
+     * </ul>
      * @param tileLayer The TileLayer
      * @param renderer The TileRenderer
      * @param startZoom The start zoom level
      * @param endZoom The end zoom level
      */
-    void generate(TileLayer tileLayer, TileRenderer renderer, int startZoom, int endZoom) {
+    void generate(Map options = [:], TileLayer tileLayer, TileRenderer renderer, int startZoom, int endZoom) {
         (startZoom..endZoom).each {zoom ->
             if (verbose) println "Zoom Level ${zoom}"
             long startTime = System.nanoTime()
-            tileLayer.tiles(zoom).eachWithIndex { Tile t, int i ->
+            TileCursor tileCursor = options.bounds ? tileLayer.tiles(options.bounds, zoom) : tileLayer.tiles(zoom)
+            tileCursor.eachWithIndex { Tile t, int i ->
                 if (verbose) println "   ${i}). ${t}"
                 Bounds b = tileLayer.pyramid.bounds(t)
                 if (verbose) println "          Bounds${b}"
@@ -33,7 +38,7 @@ class TileGenerator {
             }
             if (verbose) {
                 double endTime = System.nanoTime() - startTime
-                int numberOfTiles = tileLayer.pyramid.grid(zoom).size
+                int numberOfTiles = options.bounds ? tileLayer.tiles(options.bounds, zoom).size : tileLayer.pyramid.grid(zoom).size
                 println "   Generating ${numberOfTiles} tile${numberOfTiles > 1 ? 's':''} took ${endTime / 1000000000.0} seconds"
             }
         }
