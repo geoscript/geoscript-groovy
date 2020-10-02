cfeb63adc3724fb4643b0d8e1149ec955bf70b58
Jared Erickson <jared.erickson@gmail.com>
2020-10-01 17:07:58 -0700

    Add comments to Application

--- src/main/groovy/geoscript/Application.groovy
+++ src/main/groovy/geoscript/Application.groovy
@@ -4,8 +4,17 @@ import groovy.ui.GroovyMain as Script
 import groovy.ui.Console
 import org.codehaus.groovy.tools.shell.Main as Shell
 
+/**
+ * The GeoScript Application that can run scripts, shell, or console.
+ * @author Jared Erickson
+ */
 class Application {
 
+    /**
+     * Run the GeoScript Application
+     * @param args The command line arguments.  At least script, shell, or console
+     * is required.
+     */
     static void main(String[] args) {
         if (args.length < 1) {
             println "Please provide a command (script, shell, or console)"
