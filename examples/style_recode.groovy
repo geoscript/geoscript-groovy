a72066d5c25da6c3a0218beed929d6ee0313f4ec
Jared Erickson <jared.erickson@gmail.com>
2022-06-01 15:59:07 -0700

    Fix style recode example

--- examples/style_recode.groovy
+++ examples/style_recode.groovy
@@ -3,10 +3,12 @@ import geoscript.filter.Function
 import geoscript.render.Draw
 import geoscript.style.*
 
+import javax.imageio.ImageIO
+
 shp = new Shapefile("states.shp")
 func = new Function("Recode(SUB_REGION,'N Eng','#6495ED','Mid Atl','#B0C4DE','S Atl','#00FFFF','E N Cen','#9ACD32','E S Cen','#00FA9A','W N Cen','#FFF8DC','W S Cen','#F5DEB3','Mtn','#F4A460','Pacific','#87CEEB')")
 
 shp.style = new Fill(func) + new Stroke("#999999",0.1) + new Label("STATE_ABBR").point([0.5,0.5])
 shp.style.asSLD()
 
-Draw.drawToImage(shp)
+ImageIO.write(Draw.drawToImage(shp), "png", new File("states_recode.png"))
